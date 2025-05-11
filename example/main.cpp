6ca168ce4118ac5a0ed3e313fb0e7b82d6ee3266
Tiago Silva <tiagodepaulasilva@gmail.com>
2025-05-11 10:48:36 -0300

    Release v1.1

--- example/main.cpp
+++ example/main.cpp
@@ -10,11 +10,7 @@ void setup() {
 
     Wire.begin(48, 47); // SDA, SCL pins or use Wire.begin() for default pins
 
-    time_t now = 1746709284;
-    if (rtc.set_epoch(now))
-        Serial.println("RTC time written successfully.");
-    else
-        Serial.println("Failed to write time to RTC.");
+    rtc.set_epoch(1746709284);
 }
 
 void loop() {
@@ -22,8 +18,7 @@ void loop() {
     time_t now = rtc.get_epoch();
 
     struct tm *tm = gmtime(&now);
-    Serial.printf(
-        "%04d-%02d-%02d %02d:%02d:%02d UTC (%ld)\n",
+    Serial.printf("%04d-%02d-%02d %02d:%02d:%02d UTC (%ld)\n",
         tm->tm_year + 1900,
         tm->tm_mon + 1,
         tm->tm_mday,
